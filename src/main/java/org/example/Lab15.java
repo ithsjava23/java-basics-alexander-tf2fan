2c3436e66c6e9585476b70dd5fc17582c45d6950
alexander <alexanderwettergren@hotmail.com>
2023-09-15 09:30:26 +0200

    Test

--- src/main/java/org/example/Lab15.java
+++ src/main/java/org/example/Lab15.java
@@ -13,11 +13,10 @@ public class Lab15 {
         String timme;
         int minPrice = antalPriser[0];
         String minPriceTimme1 = "0";
-        String minPriceTimme2 = "0";
         int maxPrice = antalPriser[0];
         String maxPriceTimme1 = "0";
-        String maxPriceTimme2 = "0";
         int middlePrice = antalPriser[0];
+        String timma = "0";
         int sum = 0;
         int bestChargeTime;
         String menu = """
@@ -36,32 +35,37 @@ public class Lab15 {
                     case (1):
                         System.out.println("Skriv in priserna för elen varje timme i ören");
                         for (int i = 0; i < antalPriser.length; i++) {
-                            timme = String.format("%02d - %02d",i,i+1);
+                            timme = String.format("%02d - %02d", i, i + 1);
                             System.out.println(timme);
                             antalPriser[i] = sc.nextInt();
                         }
                         continue;
                     case (2):
-                        for(int i = 0;i < antalPriser.length;i++) {
-                            if(maxPrice < antalPriser[i]) {
+                        for (int i = 0; i < antalPriser.length; i++) {
+                            if (maxPrice < antalPriser[i]) {
                                 maxPrice = antalPriser[i];
-                                maxPriceTimme1 = String.format("%02d - %02d",i,i+1);
+                                maxPriceTimme1 = String.format("%02d - %02d", i, i + 1);
 
                             }
-                            if(minPrice > antalPriser[i]) {
+                            if (minPrice > antalPriser[i]) {
                                 minPrice = antalPriser[i];
-                                minPriceTimme1 = String.format("%02d - %02d",i,i+1);
+                                minPriceTimme1 = String.format("%02d - %02d", i, i + 1);
 
                             }
                             sum += antalPriser[i];
                         }
                         middlePrice = (sum / 24);
-                        System.out.println("Högsta pris : "+ maxPriceTimme1 + " " + maxPrice +" öre/kWh");
-                        System.out.println("Lägsta pris :"+ minPriceTimme1 + " " + minPrice+" öre/kWh");
-                        System.out.println("Medel pris :"+middlePrice+" öre/kWh");
+                        System.out.println("Högsta pris : " + maxPriceTimme1 + " " + maxPrice + " öre/kWh");
+                        System.out.println("Lägsta pris :" + minPriceTimme1 + " " + minPrice + " öre/kWh");
+                        System.out.println("Medel pris :" + middlePrice + " öre/kWh");
+                        continue;
+                    case (3):
+                        for(int i = 0;i < antalPriser.length;i++) {
+                            timma = String.format("%02d - %02d",i,i+1);
+                            System.out.println(timma + " " + antalPriser[i]+ " öre");
                         }
 
-
+                }
             } else if (sc.hasNextLine()) {
                 char exit = sc.next().charAt(0);
                 switch (exit) {
